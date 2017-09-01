adad5bbf02f9004a3a57c2596b917510db00f1e5
brunofarache <bruno.farache@liferay.com>
2017-09-01 11:51:18 -0300

    first commit

---
+++ app/src/main/java/com/liferay/smartbag/MainActivity.java
@@ -0,0 +1,21 @@
+package com.liferay.smartbag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SystemRequirementsChecker.checkWithDefaultDialogs(this);
+	}
+}
