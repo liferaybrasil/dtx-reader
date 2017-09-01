51215d49a5510e301487da318013aa11a5942c75
brunofarache <bruno.farache@liferay.com>
2017-09-01 13:35:06 -0300

    change from application to activity

--- app/src/main/java/com/liferay/smartbag/MainApplication.java
+++ app/src/main/java/com/liferay/smartbag/MainApplication.java
@@ -2,87 +2,11 @@ package com.liferay.smartbag;
 
 import android.app.Application;
 
-import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
-import com.estimote.coresdk.recognition.packets.Beacon;
-import com.estimote.coresdk.service.BeaconManager;
-import com.estimote.coresdk.service.BeaconManager.BeaconMonitoringListener;
-import com.estimote.coresdk.service.BeaconManager.ServiceReadyCallback;
-import com.wedeploy.android.WeDeploy;
-
-import org.json.JSONObject;
-
-import java.util.List;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-
 public class MainApplication extends Application {
-
-	private BeaconManager beaconManager;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
-//		sendStatus();
-//		detectBeacon();
 	}
 
-	private void detectBeacon() {
-		beaconManager = new BeaconManager(getApplicationContext());
-		beaconManager.setBackgroundScanPeriod(5000, 5000);
-		beaconManager.connect(new ServiceReadyCallback() {
-
-			@Override
-			public void onServiceReady() {
-				beaconManager.startMonitoring(
-					new BeaconRegion("bag", null, null, null));
-			}
-
-		});
-
-		beaconManager.setMonitoringListener(new BeaconMonitoringListener() {
-
-				@Override
-				public void onEnteredRegion(
-					BeaconRegion region, List<Beacon> beacons) {
-
-					System.out.println("entrou");
-				}
-
-				@Override
-				public void onExitedRegion(BeaconRegion region) {
-					System.out.println("saiu");
-				}
-			}
-
-		);
-	}
-
-	private void sendStatus() {
-		try {
-			WeDeploy we = new WeDeploy.Builder().build();
-
-			JSONObject status = new JSONObject();
-			status.put("status", "esteira");
-			status.put("timestamp", System.currentTimeMillis());
-
-			we.data("https://db-dtx.wedeploy.io")
-				.create("luggage", status)
-					.asSingle()
-					.subscribeOn(Schedulers.io())
-					.observeOn(AndroidSchedulers.mainThread())
-					.subscribe(
-						response -> {
-							System.out.println(response.succeeded());
-						},
-						throwable -> {
-							System.out.println(throwable.getMessage());
-						}
-					);
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
 }
--- app/src/main/java/com/liferay/smartbag/StatusActivity.java
+++ app/src/main/java/com/liferay/smartbag/StatusActivity.java
@@ -4,7 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
+import com.estimote.coresdk.service.BeaconManager.BeaconMonitoringListener;
+import com.wedeploy.android.WeDeploy;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 public class StatusActivity extends AppCompatActivity {
+
+	private BeaconManager beaconManager;
+	private String _status;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +30,67 @@ public class StatusActivity extends AppCompatActivity {
 		Bundle extras = getIntent().getExtras();
 
 		if (extras != null) {
-			String status = extras.getString("status");
+			_status = extras.getString("status");
 			TextView textView = (TextView)findViewById(R.id.status);
-			textView.setText(status);
+			textView.setText(_status);
+		}
+
+		detectBeacon();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		beaconManager.disconnect();
+	}
+
+	private void detectBeacon() {
+		beaconManager = new BeaconManager(getApplicationContext());
+		beaconManager.setBackgroundScanPeriod(5000, 5000);
+		beaconManager.connect(() -> beaconManager.startMonitoring(
+			new BeaconRegion("bag", null, null, null)));
+
+		beaconManager.setMonitoringListener(new BeaconMonitoringListener() {
+
+		    @Override
+		    public void onEnteredRegion(
+	            BeaconRegion region, List<Beacon> beacons) {
+
+		        sendStatus();
+		    }
+
+		    @Override
+		    public void onExitedRegion(BeaconRegion region) {
+		        System.out.println("saiu");
+		    }
+		});
+	}
+
+	private void sendStatus() {
+		try {
+			WeDeploy we = new WeDeploy.Builder().build();
+
+			JSONObject status = new JSONObject();
+			status.put("status", _status);
+			status.put("timestamp", System.currentTimeMillis());
+
+			we.data("https://db-dtx.wedeploy.io")
+				.create("luggage", status)
+				.asSingle()
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(
+					response -> {
+						System.out.println(response.succeeded());
+					},
+					throwable -> {
+						System.out.println(throwable.getMessage());
+					}
+				);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
+
 }
